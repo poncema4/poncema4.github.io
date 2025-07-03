112435d62b084238e04043369b027a3fec9a545f
Marco Ponce <marcpon8@gmail.com>
2025-07-03 11:37:08 -0400

    audit change

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/poncema4.github.io/',
   server: {
     host: "::",
     port: 8080,
