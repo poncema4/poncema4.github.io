76fe389dbda197d2c86b78c13e63ee0be5635507
Marco Ponce <marcpon8@gmail.com>
2025-10-17 20:27:51 -0400

    Fix for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/poncema4.github.io/',
+  base: '/',
   server: {
     host: "::",
     port: 8080,
